48945b8a5e6abedb694de3ed70d12995755040f6
mrodriguezzsx <mrodrigu@jaumebalmes.net>
2022-11-16 19:24:04 +0100

    Part 2 100% HECHA

--- buscamines.js
+++ buscamines.js
@@ -1,32 +1,34 @@
+let mines;
 
-function inicialitzaJoc() {
+function creaTaula() {
     let body = document.getElementsByTagName("body")[0];
     let div = document.createElement("div");
 
     //Elements taula
     let Taula = document.createElement("table");
-    Taula.style.width = "33%";
-    Taula.style.border = "1px solid black";
+    Taula.style.width = "50%";
+    Taula.style.border = "3px solid black";
     Taula.style.padding = "2px";
     Taula.style.marginTop = "5px";
 
     let TaulaBody = document.createElement("tbody");
-    TaulaBody.style.border = "1px solid black";
+    TaulaBody.style.border = "3px solid black";
 
     if (document.getElementsByTagName("table").length!=0){
         //Es borra aquest element
         document.getElementsByTagName("table")[0].remove();
     }
 
-    for (let j = 0; j < document.getElementById("row").value; j++) {
+    for (let j = 0; j < document.getElementById("midaX").value; j++) {
         // Craecio row's de la taula
         let row = document.createElement("tr");
 
-        for (let i = 0; i < document.getElementById("td").value; i++) {
+        for (let i = 0; i < document.getElementById("midaY").value; i++) {
             // Creacio cel·les de la taula
             let td = document.createElement("td");
             td.innerHTML = "&nbsp";
-            td.style.border = "1px solid black";
+            td.style.border = "2px solid black";
+            td.style.margin = "1px";
             row.appendChild(td);
         }
         TaulaBody.appendChild(row);
@@ -34,7 +36,8 @@ function inicialitzaJoc() {
 
     Taula.appendChild(TaulaBody);
     div.appendChild(Taula)
-    body.appendChild(div);    
+    body.appendChild(div);
+
 }
 
 function matriuBinaria(matrix) {
@@ -52,4 +55,51 @@ function matriuBinaria(matrix) {
         matrix2.push(fila);
     }
     return matrix2;
+}
+
+function inicialitzaMines(nMines, midaX , midaY) {
+
+    let matriu = [];
+    let numMines = nMines;
+    for (let n = 0; n < midaX; n++) {
+        let fila = [];
+        for (let m = 0; m < midaY; m++) {
+            fila.push(0)
+        }
+        matriu.push(fila);
+    }
+    while (numMines != 0) {
+        let n = Math.trunc(Math.random() * (midaX));
+        let m = Math.trunc(Math.random() * (midaY));
+        if (matriu[n][m] != 1) {
+            matriu[n][m] = 1;
+            numMines--;
+        }
+    }
+    return matriu;
+}
+
+
+function pintamines(mines) {
+    let taula = document.getElementsByTagName("tbody")[0];
+    for (let n = 0; n < mines.length; n++) {
+        for (let m = 0; m < mines[0].length; m++) {
+            if (mines[n][m] == 1) {
+                taula.children[n].children[m].style.backgroundColor = "#F3DDF2";
+
+            }
+        }
+    }
+}
+
+
+function inicialitzaJoc() {
+    creaTaula();
+
+    let nMines = document.getElementById("nMines").valueAsNumber;
+    let midaX = document.getElementById("midaX").valueAsNumber;
+    let midaY = document.getElementById("midaY").valueAsNumber;
+    mines = inicialitzaMines(nMines, midaX, midaY);
+
+    pintamines(mines);
 }
